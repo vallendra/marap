d1af1fefef7704d2e34acfc54c2fc74fdd1ee605
vallendra <kyky.farly@ralali.com>
2019-10-13 15:50:51 +0700

    rename button folder

--- src/__test__/components/Button.spec.js
+++ src/__test__/components/Button.spec.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { cleanup } from '@testing-library/react';
 import { renderWithThemeProvider } from 'test-utils';
-import Button from './index';
+import Button from 'components/Button';
 
 beforeEach(cleanup);
 
